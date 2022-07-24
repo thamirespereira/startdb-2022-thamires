37cdc5028a189cd8b53eb5aa344eb57af960c5a3
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2022-07-23 23:31:14 -0300

    Update forca.js

--- desafio/forca.js
+++ desafio/forca.js
@@ -2,7 +2,31 @@ class Forca {
 
   chutar(letra) { }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  var palavraSecreta = ['a', 'b', 'a', 'c', 'a', 'x', 'i'];
+  var letra = chute;
+  var acertos =[];
+  var erros =[];
+  var errosMax = 6;
+  var vidas = 6;
+  var addAcertos = acertos.push();
+  var addErros = erros.push();
+
+if (letra && ){
+		addAcertos;
+		console.log("parabéns, você acertou!");
+	} else{
+		addErros; 
+		vidas = vidas - 1; 
+		console.log("você errou.");
+	}
+
+if (vidas > 0 && acertos === palavraSecreta) {
+	console.log ("ganhou");
+} 
+	var letrasChutadas = acertos.concat(erros);
+}
+  
+  buscarEstado() { return ["perdeu", "ganhou", "aguardando chute"].includes(jogo.buscarDadosDoJogo); } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
       return {
